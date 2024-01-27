d967f615b525a712413d03353e5d47ba6e4adace
Varad Deshmukh <varaddeshmukh18@gmail.com>
2024-01-28 02:24:19 +0530

    Login page ui with basic navigation

--- app/src/main/java/com/vaddy/pceterpapp/MainActivity.kt
+++ app/src/main/java/com/vaddy/pceterpapp/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.vaddy.pceterpapp.loginUi.Login
 import com.vaddy.pceterpapp.navigationGraph.NavDisplay
 import com.vaddy.pceterpapp.ui.theme.PCETERPAppTheme
 
