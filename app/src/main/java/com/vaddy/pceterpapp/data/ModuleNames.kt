850dc2046e927ee46431e12ed0c5420185ee495e
Varad Deshmukh <varaddeshmukh18@gmail.com>
2024-01-30 23:32:09 +0530

    Data class update

---
+++ app/src/main/java/com/vaddy/pceterpapp/data/ModuleNames.kt
@@ -0,0 +1,20 @@
+package com.vaddy.pceterpapp.data
+
+import com.vaddy.pceterpapp.R
+
+data class ModuleNames(
+    var name : String,
+    var icon : Int,
+)
+
+var ModuleList = listOf(
+    ModuleNames("Academics", R.drawable.academics),
+    ModuleNames("Accounts", R.drawable.fees),
+    ModuleNames("Certificate", R.drawable.certificate),
+    ModuleNames("Examination", R.drawable.lms),
+    ModuleNames("Feedback", R.drawable.feedback),
+    ModuleNames("Hostel", R.drawable.hostel),
+    ModuleNames("My Guardian", R.drawable.guardian),
+    ModuleNames("My Time Table", R.drawable.mytimetable),
+    ModuleNames("Subject Registration", R.drawable.subjectregistration),
+)
